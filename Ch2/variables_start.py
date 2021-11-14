6373bd59bffcfa16b4bba1ae9c8d4df64b6db562
MavFeuille <mavis.feuille@gmail.com>
2021-11-14 14:14:07 +0800

    Complete variables & expressions

--- Ch2/variables_start.py
+++ Ch2/variables_start.py
@@ -7,12 +7,11 @@ f=0
 # print(f)
 
 
-# # re-declaring the variable works
-# f='abc'
+# # # re-declaring the variable works
+# f="abc"
 # print(f)
 
-
-# # ERROR: variables of different types cannot be combined
+# # # ERROR: variables of different types cannot be combined
 # print("this is a string " + str(123))
 
 
