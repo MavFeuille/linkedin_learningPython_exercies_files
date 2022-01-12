e9a5292adb8bac0e92de3dac2925665d7edd39b6
MavFeuille <mavis.feuille@gmail.com>
2022-01-12 11:38:45 -0500

    use the break and continue statements

--- Ch2/loops_start.py
+++ Ch2/loops_start.py
@@ -15,12 +15,14 @@ def main():
   #   print(x)
 
   # use a for loop over a collection
-  days=["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-  for d in days:
-    print(d)
+  # days=["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+  # for d in days:
+  #   print(d)
   
   # use the break and continue statements
- 
+  for x in range(5, 10):
+    if (x ==7): break
+    print(x) 
 
   #using the enumerate() function to get index 
   # days=["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
