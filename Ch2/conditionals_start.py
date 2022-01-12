38177c119d589739e1be6ad954ee0ba317d1c934
MavFeuille <mavis.feuille@gmail.com>
2022-01-12 11:28:18 -0500

    Define conditional statements

--- Ch2/conditionals_start.py
+++ Ch2/conditionals_start.py
@@ -4,10 +4,18 @@
 
 
 def main():
-    x, y = 10, 100
+    x, y = 1000, 100
 
     # conditional flow uses if, elif, else
-   
+    if (x < y):
+        st = "x is less than y"
+    else:
+        st = "x is greater than y"
+    
+    print (st)
+
+        
+
 
 
     # conditional statements let you use "a if C else b" (like ternary operator in JS)
