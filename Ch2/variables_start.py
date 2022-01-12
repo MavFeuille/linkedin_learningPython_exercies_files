74bdd56146a153185bbac174c0bc709358fefdf4
MavFeuille <mavis.feuille@gmail.com>
2022-01-12 11:04:27 -0500

    Reset variables_start.py to blank

--- Ch2/variables_start.py
+++ Ch2/variables_start.py
@@ -2,27 +2,15 @@
 # Example file for variables
 #
 
-# # Declare a variable and initialize it
-f=0
-# print(f)
+# Declare a variable and initialize it
 
 
-# # # re-declaring the variable works
-# f="abc"
-# print(f)
 
-# # # ERROR: variables of different types cannot be combined
-# print("this is a string " + str(123))
+# re-declaring the variable works
+
+
+# ERROR: variables of different types cannot be combined
+
 
 
 # Global vs. local variables in functions
-def someFunction():
-    global f
-    f="def"
-    print(f)
-
-someFunction()
-print(f)
-
-del f
-print(f)
