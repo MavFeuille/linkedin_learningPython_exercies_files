6f42907e0cc828ab138157230a79f9c341ea2798
MavFeuille <mavis.feuille@gmail.com>
2022-01-12 15:21:19 -0500

    Declare class with methods

--- Ch2/classes_start.py
+++ Ch2/classes_start.py
@@ -1,9 +1,17 @@
 #
 # Example file for working with classes
 #
+class myClass():
+    def method1(self):
+        print("myClass method1")
 
+    def method2(self, someString):
+        print("myClass method2" + someString)
 
 def main():
+    c = myClass()
+    c.method1()
+    c.method2("This is a string")
     
 
 
