96338e116c6962008a2679e3b2c23be09e35c32c
MavFeuille <mavis.feuille@gmail.com>
2022-01-12 11:25:53 -0500

    Reset file to blank

--- Ch2/conditionals_start.py
+++ Ch2/conditionals_start.py
@@ -7,19 +7,11 @@ def main():
     x, y = 10, 100
 
     # conditional flow uses if, elif, else
-    if (x < y):
-        st = "x is less than y"
-    elif (x == y):
-        st = "x is the same as y"
-    else:
-        st = "x is greater than y"
-
+   
 
 
     # conditional statements let you use "a if C else b" (like ternary operator in JS)
-    st = "x is less than y" if (x < y) else "x is greater or the same as y"
-
-    print (st)
+    
 
 if __name__ == "__main__":
     main()
