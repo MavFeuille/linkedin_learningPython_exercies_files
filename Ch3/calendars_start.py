50c91ec1272f8b7d55cd4d53b759de47f7b522af
MavFeuille <mavis.feuille@gmail.com>
2022-01-13 16:15:28 -0500

    Provides calendar month and day name

--- Ch3/calendars_start.py
+++ Ch3/calendars_start.py
@@ -6,20 +6,27 @@
 import calendar
 
 # create a plain text calendar
-c = calendar.TextCalendar(calendar.MONDAY)
-st = c.formatmonth(2022, 1, 0, 0)
-print(st)
+c = calendar.TextCalendar(calendar.SUNDAY)
+# st = c.formatmonth(2022, 1, 0, 0)
+# print(st)
 
 # create an HTML formatted calendar
-
+# hc = calendar.HTMLCalendar(calendar.SUNDAY)
+# st = hc.formatmonth(2022, 1)
+# print(st)
 
 # loop over the days of a month
 # zeroes mean that the day of the week is in an overlapping month
-
+# for i in c.itermonthdays(2021, 1):
+#   print(i)
   
 # The Calendar module provides useful utilities for the given locale,
 # such as the names of days and months in both full and abbreviated forms
+for name in calendar.month_name:
+    print(name)
 
+for day in calendar.day_name:
+    print(day)
 
 
 
