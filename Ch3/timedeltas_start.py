49458ebc8182af77878db83626a022d6979b56e5
MavFeuille <mavis.feuille@gmail.com>
2022-01-13 14:54:41 -0500

    Construct a basic timedelta

--- Ch3/timedeltas_start.py
+++ Ch3/timedeltas_start.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from datetime import timedelta
 
 # construct a basic timedelta and print it
-
+print(timedelta(days=365, hours=5, minutes=1))
 
 
 # print today's date
