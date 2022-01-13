c043de7816a60ee2d217d0f7bb1bb49f987c8ea0
MavFeuille <mavis.feuille@gmail.com>
2022-01-13 16:23:45 -0500

    Find out the meetday of each month

--- Ch3/calendars_start.py
+++ Ch3/calendars_start.py
@@ -22,11 +22,11 @@ c = calendar.TextCalendar(calendar.SUNDAY)
   
 # The Calendar module provides useful utilities for the given locale,
 # such as the names of days and months in both full and abbreviated forms
-for name in calendar.month_name:
-    print(name)
+# for name in calendar.month_name:
+#     print(name)
 
-for day in calendar.day_name:
-    print(day)
+# for day in calendar.day_name:
+#     print(day)
 
 
 
@@ -34,3 +34,15 @@ for day in calendar.day_name:
 # a team meeting on the first Friday of every month.
 # To figure out what days that would be for each month,
 # we can use this script:
+print("Team meetings will be on: ")
+for m in range(1, 13):
+    cal = calendar.monthcalendar(2022, m)
+    weekone=cal[0]
+    weektwo=cal[1]
+
+    if weekone[calendar.FRIDAY] != 0:
+        meetday = weekone[calendar.FRIDAY]
+    else:
+        meetday = weektwo[calendar.FRIDAY]
+   
+    print("%10s %2d" % (calendar.month_name[m], meetday))
