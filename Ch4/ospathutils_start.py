dd0e80d35b01e577b73f386a51ebd7288d5fb6e4
MavFeuille <mavis.feuille@gmail.com>
2022-01-14 20:24:34 -0500

    Work with file paths

--- Ch4/ospathutils_start.py
+++ Ch4/ospathutils_start.py
@@ -10,15 +10,16 @@ import time
 
 def main():
   # Print the name of the OS
-  print(os.name)
+  # print(os.name)
   
   # Check for item existence and type
-  print("Item exists: " + str(path.exists("textfile.txt")))
-  print("Item is a file: " + str(path.isfile("textfile.txt")))
-  print("Item is a directory: " + str(path.isdir("textfile.txt")))
+  # print("Item exists: " + str(path.exists("textfile.txt")))
+  # print("Item is a file: " + str(path.isfile("textfile.txt")))
+  # print("Item is a directory: " + str(path.isdir("textfile.txt")))
 
   # Work with file paths
-
+  print("Item path: " + str(path.realpath("textfile.txt")))
+  print("Item path and name:" + str(path.split(path.realpath("textfile.txt"))))
   
   # Get the modification time
 
