a2378b5562be85e9785e7e91f33b567df02e5d85
MavFeuille <mavis.feuille@gmail.com>
2022-01-14 20:18:59 -0500

    Open file and read contents in lines

--- Ch4/files_start.py
+++ Ch4/files_start.py
@@ -7,17 +7,24 @@ def main():
   # f = open("textfile.txt", "w+") #<- which file, and "w+" means write
   
   # Open the file for appending text to the end
-  f = open("textfile.txt", "a") #<- which file and "a" means append
+  f = open("textfile.txt", "r") #<- which file and "a" means append, "r" = read
 
   # write some lines of data to the file
-  for i in range(10):
-    f.write("This is line " + str(i) + "\r\n")
+  # for i in range(10):
+  #   f.write("This is line " + str(i) + "\r\n")
 
   # close the file when done
-  f.close()
+  # f.close()
   
   # Open the file back up and read the contents
+  if f.mode == 'r':
+    # contents = f.read()
+    fl = f.readlines()
+    for x in fl:
+      print(x)
 
+
+    # print(contents)
     
 if __name__ == "__main__":
   main()
