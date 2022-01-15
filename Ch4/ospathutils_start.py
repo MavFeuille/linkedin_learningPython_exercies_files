7e74e103a2b16d1947a861eb1ca90b31fb178a89
MavFeuille <mavis.feuille@gmail.com>
2022-01-14 20:22:50 -0500

    Check for item existence and type

--- Ch4/ospathutils_start.py
+++ Ch4/ospathutils_start.py
@@ -13,8 +13,10 @@ def main():
   print(os.name)
   
   # Check for item existence and type
+  print("Item exists: " + str(path.exists("textfile.txt")))
+  print("Item is a file: " + str(path.isfile("textfile.txt")))
+  print("Item is a directory: " + str(path.isdir("textfile.txt")))
 
-  
   # Work with file paths
 
   
