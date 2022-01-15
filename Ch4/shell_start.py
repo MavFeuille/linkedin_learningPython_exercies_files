08c06cc567c489f933ae3e29bb04a2557e2ea7ec
MavFeuille <mavis.feuille@gmail.com>
2022-01-14 20:41:52 -0500

    Add textfile.txt and "bak" into zip file with shutil

--- Ch4/shell_start.py
+++ Ch4/shell_start.py
@@ -5,6 +5,7 @@ import os
 from os import path
 import shutil
 from shutil import make_archive
+from zipfile import ZipFile
 
 def main():
   # make a duplicate of an existing file
@@ -23,10 +24,13 @@ def main():
     # os.rename("textfile.txt", "newfile.txt")
     
     # now put things into a ZIP archive
-
+    # root_dir, tail = path.split(src)
+    # shutil.make_archive("archive", "zip", root_dir)
 
     # more fine-grained control over ZIP files
-
+    with ZipFile("testzip.zip", "w") as newzip:
+      newzip.write("textfile.txt")
+      newzip.write("textfile.txt.bak")
       
 if __name__ == "__main__":
   main()
