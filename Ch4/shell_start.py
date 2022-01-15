017177ecbdc10ee487c9a4a4e3eb24d6270e3fcd
MavFeuille <mavis.feuille@gmail.com>
2022-01-14 20:35:18 -0500

    Rename the original file

--- Ch4/shell_start.py
+++ Ch4/shell_start.py
@@ -15,11 +15,11 @@ def main():
     dst = src + ".bak"
     
     # copy over the permissions, modification times, and other info
-    shutil.copy(src, dst)
-    shutil.copystat(src, dst)
+    # shutil.copy(src, dst)
+    # shutil.copystat(src, dst)
     
     # rename the original file
-
+    os.rename("textfile.txt", "newfile.txt")
     
     # now put things into a ZIP archive
 
