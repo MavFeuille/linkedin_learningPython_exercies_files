b6736163442667d0237966503bf9c8d2c9ee5255
MavFeuille <mavis.feuille@gmail.com>
2022-01-16 21:53:41 -0500

    Set up variable to parse samplexml.xml

--- Ch5/xmlparsing_start.py
+++ Ch5/xmlparsing_start.py
@@ -3,9 +3,9 @@
 #
 import xml.dom.minidom
 
-def main:
+def main():
   # use the parse() function to load and parse an XML file
-
+  doc = xml.dom.minidom.parse("samplexml.xml")
   
   # print out the document node and the name of the first child tag
 
