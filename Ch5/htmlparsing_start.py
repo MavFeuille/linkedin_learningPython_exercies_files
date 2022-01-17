cdf2fde2ec4c91ad6e46394146ccbdb69ddd816f
MavFeuille <mavis.feuille@gmail.com>
2022-01-16 21:46:59 -0500

    Set metacount as global variable

--- Ch5/htmlparsing_start.py
+++ Ch5/htmlparsing_start.py
@@ -3,6 +3,8 @@
 #
 from html.parser import HTMLParser
 
+metacount = 0;
+
 class MyHTMLParser(HTMLParser):
   def handle_comment(self, data):
     print("Encountered comment: ", data)
@@ -10,14 +12,25 @@ class MyHTMLParser(HTMLParser):
     print("\tAt line: ", pos[0], "position", pos[1])
 
   def handle_starttag(self, tag, attrs):
-
+    global metacount
+    if tag == "meta":
+      metacount += 1
+    print("Encountered tag: ", tag)
+    pos = self.getpos()
+    print("\tAt line: ", pos[0], "position", pos[1])
 
   def handle_endtag(self, tag):
-
+    print("Encountered tag: ", tag)
+    pos = self.getpos()
+    print("\tAt line: ", pos[0], "position", pos[1])
 
   def handle_data(self, data):
+    if (data.isspace())
+      return
+    print("Encountered data: ", data)
+    pos = self.getpos()
+    print("\tAt line: ", pos[0], "position", pos[1])
 
-    
 def main():
   # instantiate the parser and feed it some HTML
   parser = MyHTMLParser()
