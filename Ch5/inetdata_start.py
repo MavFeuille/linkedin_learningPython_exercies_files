13f2958cb41a2b55269eed0b73c815ae075c449e
MavFeuille <mavis.feuille@gmail.com>
2022-01-16 21:15:41 -0500

    Import urllib.request

--- Ch5/inetdata_start.py
+++ Ch5/inetdata_start.py
@@ -2,6 +2,10 @@
 # Example file for retrieving data from the internet
 #
 
+import urllib.request
+
+def main():
+  webUrl = urllib.request.urlopen("http://www.google.com")
 
 if __name__ == "__main__":
   main()
